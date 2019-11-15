96477152f1ad453c856b123da87ef283c5080c7b
antoinesorriaux <antoine.sorriaux@gmail.com>
2019-11-15 23:14:54 +0100

    Fiche

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Fiche from './Fiche.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Fiche />
+    </div>
+  );
+}
+
+export default App;
